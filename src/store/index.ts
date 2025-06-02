64aa8c31f9472bcb3b759bade957e0e2bdf40b0a
Mantey <nanakofilarbimantey@gmail.com>
2025-06-02 20:45:40 +0000

    adds setting page with theme switcher and profile info

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,5 +2,6 @@ import { useOnboardingStore } from './useOnboarding'
 import { useSessionStore } from './useSession'
 import { useActiveTabStore } from './useActiveTab'
 import { useProfileStore } from './useProfile'
+import { useThemeStore } from './useTheme'
 
-export { useSessionStore, useActiveTabStore, useOnboardingStore, useProfileStore }
+export { useSessionStore, useActiveTabStore, useOnboardingStore, useProfileStore, useThemeStore }
